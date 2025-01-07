0da1a6cec5c6c11ea9409328ece8b8ea8ba1e5a9
zidane.nur004 <zidane.nur004@gmail.com>
2025-01-07 01:11:54 +0000

    little fix to deploy

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,14 +1,14 @@
 // import { sql } from "@vercel/postgres";
 // import { UserType } from "../lib/defintion";
-// import { fetchAllFoods, fetchCartsByNote, fetchDisplayCart, fetchFoodsByCategory, fetchNotesById, fetchUserById } from "../lib/data";
+import { fetchFoodsByCategory } from "../lib/data";
 
-// export async function GET() {
-//     try {
+export async function GET() {
+    try {
 
-//         const result = await fetchFoodsByCategory(['drink', 'food', 'dessert']);
+        const result = await fetchFoodsByCategory(['drink', 'food', 'dessert']);
 
-//         return Response.json({ result });
-//     } catch (error) {
-//         return Response.json({ error }, { status: 500});
-//     }
-// }
+        return Response.json({ result });
+    } catch (error) {
+        return Response.json({ error }, { status: 500});
+    }
+}
